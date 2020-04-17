237b31016d9fa2d96e7227936a5e567cb95fce00
Olga Sadova <sadovaolga1@gmail.com>
2020-04-16 17:21:45 -0700

    Create JAWSDB_URL

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,37 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Soa15091987",
-    database: "burgers_db"
-});
+var connecting;
 
-// Make connection.
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+if (process.env.JAWSDB_URL) {
+    connecting = mysql.createConnection(process.env.JAWSDB_URL);
+
+} else {
+    connecting = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'Soa15091987',
+        database: 'burgers_db'
+    });
+};
+
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "Soa15091987",
+//     database: "burgers_db"
+// });
+
+// // Make connection.
+// connection.connect(function(err) {
+//     if (err) {
+//         console.error("error connecting: " + err.stack);
+//         return;
+//     }
+//     console.log("connected as id " + connection.threadId);
+// });
 
 // Export connection for our ORM to use.
+connecting.connect();
 module.exports = connection;
